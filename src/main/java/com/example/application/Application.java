eb222a8d49ba9afbf27be17c46467bec94b5b933
JayBlaine <jbrags42@gmail.com>
2021-05-06 15:17:36 -0500

    encryption v0.3

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -40,122 +40,152 @@ public static SQLConnect pgm;
 		String crypTxt = "";
 		for (int i=0;i<pass.length();i++)  
         {  
-			if(i % 4 == 0)
-				continue;
+			//if(i % 4 == 0)
+			//	continue;
             char ch=Character.toLowerCase(pass.charAt(i));  
             switch (ch)  
             {  
-                case '{':  
-                    crypTxt=crypTxt+"A";  
-                    break;  
-                case '}':  
-                    crypTxt=crypTxt+"B";  
-                    break;  
-                case '#':  
-                    crypTxt=crypTxt+"C";  
-                    break;  
-                case '~':  
-                    crypTxt=crypTxt+"D";  
-                    break;  
-                case '+':  
-                    crypTxt=crypTxt+"E";  
-                    break;  
-                case '-':  
-                    crypTxt=crypTxt+"F";  
-                    break;  
-                case '*':  
-                    crypTxt=crypTxt+"G";  
-                    break;  
-                case '@':  
-                    crypTxt=crypTxt+"H";  
-                    break;  
-                case '/':  
-                    crypTxt=crypTxt+"I";  
-                    break;  
-                case '\\':  
-                    crypTxt=crypTxt+"J";  
-                    break;  
-                case '?':  
-                    crypTxt=crypTxt+"K";  
-                    break;  
-                case '$':  
-                    crypTxt=crypTxt+"L";  
-                    break;  
-                case '!':  
-                    crypTxt=crypTxt+"M";  
-                    break;  
-                case '^':  
-                    crypTxt=crypTxt+"N";  
-                    break;  
-                case '(':  
-                    crypTxt=crypTxt+"O";  
-                    break;  
-                case ')':  
-                    crypTxt=crypTxt+"P";  
-                    break;  
-                case '<':  
-                    crypTxt=crypTxt+"Q";  
-                    break;  
-                case '>':  
-                    crypTxt=crypTxt+"R";  
-                    break;  
-                case '=' :  
-                    crypTxt=crypTxt+"S";  
-                    break;  
-                case ';':  
-                    crypTxt=crypTxt+"T";  
-                    break;  
-                case ',':  
-                    crypTxt=crypTxt+"U";  
-                    break;  
-                case '_' :  
-                    crypTxt=crypTxt+"V";  
-                    break;  
-                case '[':  
-                    crypTxt=crypTxt+"W";  
-                    break;  
-                case ']' :  
-                    crypTxt=crypTxt+"X";  
-                    break;  
-                case ':':  
-                    crypTxt=crypTxt+"Y";  
-                    break;  
-                case '\"' :  
-                    crypTxt=crypTxt+"Z";  
-                    break;       
-                case '1':  
-                    crypTxt=crypTxt+"r";  
-                    break;  
-                case '2':  
-                    crypTxt=crypTxt+"k";  
-                    break;  
-                case '3':  
-                    crypTxt=crypTxt+"b";  
-                    break;  
-                case '4':  
-                    crypTxt = crypTxt+"e";  
-                    break;  
-                case '5':  
-                    crypTxt = crypTxt+"q";  
-                    break;  
-                case '6':  
-                    crypTxt = crypTxt+"h";  
-                    break;  
-                case '7':  
-                    crypTxt = crypTxt+"u";  
-                    break;  
-                case '8' :  
-                    crypTxt= crypTxt+"y";  
-                    break;  
-                case '9':  
-                    crypTxt = crypTxt+"w";  
-                    break;  
-                case '0':  
-                    crypTxt = crypTxt+"z";  
-                    break;  
-                 default:  
-                    crypTxt=crypTxt+"0";  
-                    break;  
+            case '{':  
+                crypTxt=crypTxt+"a";  
+                break;  
+            case '}':  
+                crypTxt=crypTxt+"b";  
+                break;  
+            case '#':  
+                crypTxt=crypTxt+"c";  
+                break;  
+            case '~':  
+                crypTxt=crypTxt+"d";  
+                break;  
+            case '+':  
+                crypTxt=crypTxt+"e";  
+                break;  
+            case '-':  
+                crypTxt=crypTxt+"f";  
+                break;  
+            case '*':  
+                crypTxt=crypTxt+"g";  
+                break;  
+            case '@':  
+                crypTxt=crypTxt+"h";  
+                break;  
+            case '/':  
+                crypTxt=crypTxt+"i";  
+                break;  
+            case '\\':  
+                crypTxt=crypTxt+"j";  
+                break;  
+            case '?':  
+                crypTxt=crypTxt+"k";  
+                break;  
+            case '$':  
+                crypTxt=crypTxt+"l";  
+                break;  
+            case '!':  
+                crypTxt=crypTxt+"m";  
+                break;  
+            case '^':  
+                crypTxt=crypTxt+"n";  
+                break;  
+            case '(':  
+                crypTxt=crypTxt+"o";  
+                break;  
+            case ')':  
+                crypTxt=crypTxt+"p";  
+                break;  
+            case '<':  
+                crypTxt=crypTxt+"q";  
+                break;  
+            case '>':  
+                crypTxt=crypTxt+"r";  
+                break;  
+            case '=' :  
+                crypTxt=crypTxt+"s";  
+                break;  
+            case ';':  
+                crypTxt=crypTxt+"t";  
+                break;  
+            case ',':  
+                crypTxt=crypTxt+"u";  
+                break;  
+            case '_' :  
+                crypTxt=crypTxt+"v";  
+                break;  
+            case '[':  
+                crypTxt=crypTxt+"w";  
+                break;  
+            case ']' :  
+                crypTxt=crypTxt+"x";  
+                break;  
+            case ':':  
+                crypTxt=crypTxt+"y";  
+                break;  
+            case '\"' :  
+                crypTxt=crypTxt+"z";  
+                break;  
+            case ' ' :  
+                crypTxt=crypTxt+" ";  
+                break;  
+            case '3':  
+                crypTxt=crypTxt+'.';  
+                break;  
+            case '1':  
+                crypTxt=crypTxt+",";  
+                break;  
+            case '4':  
+                crypTxt=crypTxt+'(';  
+                break;  
+            case '5':  
+                crypTxt=crypTxt+'\"';  
+                break;  
+            case '7' :  
+                crypTxt=crypTxt+")";  
+                break;  
+            case '2' :  
+                crypTxt= crypTxt+"?";  
+                break;  
+            case '8':  
+                crypTxt= crypTxt+"!";  
+                break;  
+            case '6' :  
+                crypTxt= crypTxt+"-";  
+                break;  
+            case '9' :  
+                crypTxt = crypTxt+"%";  
+                break;  
+            case 'r':  
+                crypTxt=crypTxt+"1";  
+                break;  
+            case 'k':  
+                crypTxt=crypTxt+"2";  
+                break;  
+            case 'b':  
+                crypTxt=crypTxt+"3";  
+                break;  
+            case 'e':  
+                crypTxt = crypTxt+"4";  
+                break;  
+            case 'q':  
+                crypTxt = crypTxt+"5";  
+                break;  
+            case 'h':  
+                crypTxt = crypTxt+"6";  
+                break;  
+            case 'u':  
+                crypTxt = crypTxt+"7";  
+                break;  
+            case 'y' :  
+                crypTxt= crypTxt+"8";  
+                break;  
+            case 'w':  
+                crypTxt = crypTxt+"9";  
+                break;  
+            case 'z':  
+                crypTxt = crypTxt+"0";  
+                break;  
+             default:  
+                crypTxt=crypTxt+"0";  
+                break;  
             }  
         }
 		return crypTxt;  
@@ -168,8 +198,8 @@ public static SQLConnect pgm;
 		char [] randChar = {'$', '!', '^', '%', '(', ')', '3', '7', 'L', 'W', 'S'};
 		for (int i=0;i<pass.length();i++)  
         {  
-			if(i % 4 == 0)
-				crypTxt=crypTxt+randChar[rand.nextInt() % 11];
+			//if(i % 4 == 0)
+			//	crypTxt=crypTxt+randChar[rand.nextInt() % 11];
             char ch=Character.toLowerCase(pass.charAt(i));  
             switch (ch)  
             {  
